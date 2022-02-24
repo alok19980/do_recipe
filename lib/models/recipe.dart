37e0344e80cb4da92d74d20e96baf8bee7736628
aloksinghchauhan <alokchauhan.kalpi@gmail.com>
2022-02-25 03:59:49 +0530

    "Completed Basic App"

---
+++ lib/models/recipe.dart
@@ -0,0 +1,150 @@
+class Recipe {
+  String label;
+  String imageUrl;
+  int servings;
+  List<Ingredient> ingredients;
+
+  Recipe(
+    this.label,
+    this.imageUrl,
+    this.servings,
+    this.ingredients,
+  );
+
+  static List<Recipe> samples = [
+    Recipe(
+      'Chana Masala',
+      'assets/chana_masala.jpg',
+      4,
+      [
+        Ingredient(
+          1,
+          'box',
+          'Spaghetti',
+        ),
+        Ingredient(
+          4,
+          '',
+          'Frozen Meatballs',
+        ),
+        Ingredient(
+          0.5,
+          'jar',
+          'sauce',
+        ),
+      ],
+    ),
+    Recipe(
+      'Matar Paneer',
+      'assets/mater_paneer.jpeg',
+      2,
+      [
+        Ingredient(
+          1,
+          'can',
+          'Tomato Soup',
+        ),
+      ],
+    ),
+    Recipe(
+      'Sahi Chaap',
+      'assets/sahi_chaap.jpeg',
+      1,
+      [
+        Ingredient(
+          2,
+          'slices',
+          'Cheese',
+        ),
+        Ingredient(
+          2,
+          'slices',
+          'Bread',
+        ),
+      ],
+    ),
+    Recipe(
+      'Aloo Palak',
+      'assets/aloo_palak.jpeg',
+      24,
+      [
+        Ingredient(
+          4,
+          'cups',
+          'flour',
+        ),
+        Ingredient(
+          2,
+          'cups',
+          'sugar',
+        ),
+        Ingredient(
+          0.5,
+          'cups',
+          'chocolate chips',
+        ),
+      ],
+    ),
+    Recipe(
+      'Dal Fry',
+      'assets/daal_fry.jpeg',
+      1,
+      [
+        Ingredient(
+          4,
+          'oz',
+          'nachos',
+        ),
+        Ingredient(
+          3,
+          'oz',
+          'taco meat',
+        ),
+        Ingredient(
+          0.5,
+          'cup',
+          'cheese',
+        ),
+        Ingredient(
+          0.25,
+          'cup',
+          'chopped tomatoes',
+        ),
+      ],
+    ),
+    Recipe(
+      'Gulab Jamun',
+      'assets/gulab_jamun.jpeg',
+      4,
+      [
+        Ingredient(
+          1,
+          'item',
+          'pizza',
+        ),
+        Ingredient(
+          1,
+          'cup',
+          'pineapple',
+        ),
+        Ingredient(
+          8,
+          'oz',
+          'ham',
+        ),
+      ],
+    ),
+  ];
+}
+
+class Ingredient {
+  double quantity;
+  String measure;
+  String name;
+
+  Ingredient(
+    this.quantity,
+    this.measure,
+    this.name,
+  );
+}
